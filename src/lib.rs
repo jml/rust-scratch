cc4ed43122015a4c7fde821eb62b575b53737764
Jonathan Lange <jml@mumak.net>
2014-09-14 13:20:16 +0100

    add rust compiler version

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+// rustc 0.12.0-pre-nightly (5419b2ca2 2014-08-29 22:16:20 +0000)
+
 use std::collections::TreeMap;
 
 // TODO: set up cargo documentation
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// rustc 0.12.0-pre-nightly (5419b2ca2 2014-08-29 22:16:20 +0000)
 
 extern crate target;
 
